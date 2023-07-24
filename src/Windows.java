5f7f42d95f45d892d4cbaa4e456c7010c7a87bb1
Perceval <93855736+vlad13667@users.noreply.github.com>
2023-07-24 11:26:16 +0500

    Полный экран и автоопределение разрешения

--- src/Windows.java
+++ src/Windows.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Windows {
@@ -11,10 +12,12 @@ public class Windows {
         JFrame windows = new JFrame("Крестики нолики");//главное окно
         JPanel buttonsPanel = new JPanel();// Создаём панель для кнопок табличного стиля
         windows.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // кнопка закрывающая окно
-        windows.setSize(1920, 1080);//размер окна
+        Dimension sSize = Toolkit.getDefaultToolkit ().getScreenSize ();// ОПРЕДЕЛЕНИЕ РАЗРЕШЕНИЯ  экрана
+        windows.setSize(sSize);//размер окна
         windows.setLayout(new BorderLayout());// менеджер компоновки
         windows.setLocationRelativeTo(null); //окно по центру
         windows.setVisible(true); //видимость окна
+        windows.setExtendedState(Frame.MAXIMIZED_BOTH); // ОТКРЫТИЕ НА ПОЛНЫЙ ЭКРАН
             // JButton start = new JButton("Старт");
         //JButton stop = new JButton("Выход");
         // добавляем кнопки на панель
@@ -22,12 +25,19 @@ public class Windows {
         // buttonsPanel.add(stop);
         // размещаем панель на Frame (верхняя часть)
 
-        Draw game = new Draw();
-        windows.add(game);
+        // Draw game = new Draw();
+        //windows.add(game);
 
 
     }
 
 
 
+
 }
+
+/*public class estActionsListener implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+
+    }
+}*/
